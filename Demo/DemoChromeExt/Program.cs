2aa4e02eb2421385efee481b9a2bf31f77b2eb19
Luke Vo <datvm@outlook.com>
2022-12-31 02:32:15 +0700

    Added a demo for using with Blazor.BrowserExtension and Blazor.FileSystemAccess

---
+++ Demo/DemoChromeExt/Program.cs
@@ -0,0 +1,27 @@
+﻿using Blazor.JsRuntimeRedirect.Extensions;
+using KristofferStrube.Blazor.FileSystemAccess;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace DemoChromeExt;
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services
+            .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+            .AddFileSystemAccessService()
+            .AddJsRuntimeRedirect();
+
+        builder.Services.AddBrowserExtensionServices();
+        await builder.Build().RunAsync();
+    }
+}
